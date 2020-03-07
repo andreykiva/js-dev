cecddc93458c3d36ef1d927946cbfbb51a7906e4
Andrey <57412446+podmost777@users.noreply.github.com>
2020-03-07 20:27:22 +0300

    homework 6

---
+++ homework6/js/main.js
@@ -0,0 +1,26 @@
+const startButton = document.getElementById("start"),
+  budgetValue = document.querySelector(".budget-value"),
+  dayBudgetValue = document.querySelector(".daybudget-value"),
+  levelValue = document.querySelector(".level-value"),
+  expensesValue = document.querySelector(".expenses-value"),
+  optionalExpensesValue = document.querySelector(".optionalexpenses-value"),
+  incomeValue = document.querySelector(".income-value"),
+  monthSavingsValue = document.querySelector(".monthsavings-value"),
+  yearSavingsValue = document.querySelector(".yearsavings-value"),
+  expensesItems = document.querySelectorAll(".expenses-item"),
+  buttons = document.getElementsByTagName("button"),
+  expensesItemBtn = buttons[0],
+  optionalExpensesBtn = buttons[1],
+  countBudgetBtn = buttons[2],
+  optionalExpensesItem = document.querySelectorAll(".optionalexpenses-item"),
+  chooseIncome = document.querySelector('#income'),
+  savings = document.querySelector('#savings'),
+  chooseSum = document.querySelector('#choose-sum'),
+  choosePercent = document.querySelector('#choose-percent'),
+  yearValue = document.querySelector('.year-value'),
+  monthValue = document.querySelector('.month-value'),
+  dayValue = document.querySelector('.day-value');
+
+
+
+  
